042eebf8730c02cfe565e551ff1922c7f1076fbb
alah0015 <alah0015@users.noreply.github.com>
2017-12-10 17:21:38 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,11 +4,26 @@ var _close = document.getElementById('close')
 var _modal = document.getElementById('modal')
 var _gallery = document.getElementById('gallery')
 var _nav = document.getElementById("nav"),
-    closeNav = document.getElementById("close_nav");
-    //* exit button events
+var _closeNav = document.getElementById("close_nav");
+
+// nav
+closeNav.addEventListener('click', function (e) {
+    nav.classList.add('hide');
+    e.stopPropagation()
+})
+
+// nav
+nav.addEventListener('click', function () {
+    if (this.classList.contains('hide')) {
+        this.classList.remove('hide')
+    }
+})
+
+
+//* exit button events
 window.addEventListener('keyup', closeEventHandler)
 _close.addEventListener('click', closeEventHandler)
-    //* exit button function
+//* exit button function
 function closeEventHandler(e) {
     if ((e.target && e.target.classList.contains('close')) || (e.keyCode === 27 && _body.classList.contains("no-scroll"))) {
         _body.classList.remove('no-scroll')
@@ -16,25 +31,16 @@ function closeEventHandler(e) {
 }
 //* image getting hovered  and expanding
 _gallery.addEventListener('click', function (e) {
-        console.log(e)
-        if (e.target && e.target.className == "intro") {
-            var url = e.target.getAttribute("data-src")
-            _body.classList.add('no-scroll')
-            _modal.innerHTML = '<img src="' + url + '">';
-        }
-    })
-
-    closeNav.addEventListener('click', function(e) {
-      nav.classList.add('hide');
-      e.stopPropagation()
-    })
-
-    // nav
-    nav.addEventListener('click', function() {
-      if(this.classList.contains('hide')){
-        this.classList.remove('hide')
-      }
-    })
+    console.log(e)
+    if (e.target && e.target.className == "intro") {
+        var url = e.target.getAttribute("data-src")
+        _body.classList.add('no-scroll')
+        _modal.innerHTML = '<img src="' + url + '">';
+    }
+})
 
 
-    //* js ends
+
+
+
+//* js ends
